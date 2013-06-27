0c79cdf412bd41ac1fb2d241e9ca2b261a1d59b8
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2013-06-27 12:42:38 +0200

    -update components to latest versions -PHP 5.5, Apache 2.4.4 -added gd, zlib, zip, curl, mcrypt, mysql extensions -temporarily removed APC (due to some weird problems with installation)

--- misc/install.sh
+++ misc/install.sh
@@ -3,29 +3,34 @@
 OPENSHIFT_RUNTIME_DIR=$OPENSHIFT_HOMEDIR/app-root/runtime
 OPENSHIFT_REPO_DIR=$OPENSHIFT_HOMEDIR/app-root/runtime/repo
 
-# INSTALL APACHE
+# DIRECTORIES PREPARATION
 cd $OPENSHIFT_RUNTIME_DIR
 mkdir srv
 mkdir srv/pcre
 mkdir srv/httpd
 mkdir srv/php
+mkdir srv/libmcrypt
+mkdir srv/zlib
+mkdir srv/libcurl
 mkdir tmp
 cd tmp/
-wget http://ftp.halifax.rwth-aachen.de/apache/httpd/httpd-2.4.3.tar.gz
-tar -zxf httpd-2.4.3.tar.gz
-wget http://artfiles.org/apache.org/apr/apr-1.4.6.tar.gz
-tar -zxf apr-1.4.6.tar.gz
-mv apr-1.4.6 httpd-2.4.3/srclib/apr
-wget http://artfiles.org/apache.org/apr/apr-util-1.5.1.tar.gz
-tar -zxf apr-util-1.5.1.tar.gz
-mv apr-util-1.5.1 httpd-2.4.3/srclib/apr-util
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.31.tar.gz
-tar -zxf pcre-8.31.tar.gz
-cd pcre-8.31
+
+# INSTALL APACHE
+wget http://ftp.ps.pl/pub/apache//httpd/httpd-2.4.4.tar.gz
+tar -zxf httpd-2.4.4.tar.gz
+wget http://ftp.ps.pl/pub/apache//apr/apr-1.4.8.tar.gz
+tar -zxf apr-1.4.8.tar.gz
+mv apr-1.4.8 httpd-2.4.4/srclib/apr
+wget http://ftp.ps.pl/pub/apache//apr/apr-util-1.5.2.tar.gz
+tar -zxf apr-util-1.5.2.tar.gz
+mv apr-util-1.5.2 httpd-2.4.4/srclib/apr-util
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.33.tar.gz
+tar -zxf pcre-8.33.tar.gz
+cd pcre-8.33
 ./configure \
 --prefix=$OPENSHIFT_RUNTIME_DIR/srv/pcre
 make && make install
-cd ../httpd-2.4.3
+cd ../httpd-2.4.4
 ./configure \
 --prefix=$OPENSHIFT_RUNTIME_DIR/srv/httpd \
 --with-included-apr \
@@ -39,41 +44,89 @@ cd ../httpd-2.4.3
 --enable-headers
 make && make install
 cd ..
-rm -r $OPENSHIFT_RUNTIME_DIR/tmp/*.tar.gz
 
-# INSTALL PHP
-wget http://download.icu-project.org/files/icu4c/50.1/icu4c-50_1-src.tgz
-tar -zxf icu4c-50_1-src.tgz
+# INSTALL LIBMCRYPT
+wget http://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz
+tar -zxf libmcrypt-2.5.8.tar.gz
+cd libmcrypt-2.5.8
+./configure \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/libmcrypt \
+--disable-posix-threads
+make && make install
+cd ..
+
+rm -f -r libmcrypt-2.5.8
+
+# INSTALL ICU
+wget http://download.icu-project.org/files/icu4c/51.2/icu4c-51_2-src.tgz
+tar -zxf icu4c-51_2-src.tgz
 cd icu/source/
 chmod +x runConfigureICU configure install-sh
 ./configure \
---prefix=$OPENSHIFT_RUNTIME_DIR/srv/icu/
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/icu
 make && make install
 cd ../..
-wget http://de2.php.net/get/php-5.4.7.tar.gz/from/this/mirror
-tar -zxf php-5.4.7.tar.gz
-cd php-5.4.7
+rm -f -r icu
+
+# INSTALL ZLIB
+wget http://zlib.net/zlib-1.2.8.tar.gz
+tar -zxf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+./configure \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/zlib
+make && make install
+cd ..
+rm -f -r zlib-1.2.8
+
+# INSTALL CURL
+wget http://curl.haxx.se/download/curl-7.31.0.tar.gz
+tar -zxf curl-7.31.0.tar.gz
+cd curl-7.31.0
 ./configure \
 --with-libdir=lib64 \
---prefix=$OPENSHIFT_RUNTIME_DIR/srv/php/ \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/libcurl \
+--with-zlib=$OPENSHIFT_RUNTIME_DIR/srv/zlib
+make && make install
+cd ..
+rm -f -r curl-7.31.0
+
+# INSTALL PHP
+wget http://pl1.php.net/get/php-5.5.0.tar.gz/from/this/mirror
+tar -zxf php-5.5.0.tar.gz
+cd php-5.5.0
+./configure \
+LDFLAGS="-L$OPENSHIFT_RUNTIME_DIR/srv/libmcrypt/lib" \
+--with-libdir=lib64 \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/php \
 --with-config-file-path=$OPENSHIFT_RUNTIME_DIR/srv/php/etc/apache2 \
 --with-layout=PHP \
 --with-apxs2=$OPENSHIFT_RUNTIME_DIR/srv/httpd/bin/apxs \
+--with-gd \
+--with-mysql \
+--with-pdo-mysql \
+--with-mcrypt=$OPENSHIFT_RUNTIME_DIR/srv/libmcrypt \
+--with-zlib=$OPENSHIFT_RUNTIME_DIR/srv/zlib \
+--with-curl=$OPENSHIFT_RUNTIME_DIR/srv/libcurl \
+--enable-zip \
 --enable-mbstring \
 --enable-intl \
 --with-icu-dir=$OPENSHIFT_RUNTIME_DIR/srv/icu
-make && make install
+
+make LDFLAGS="-L$OPENSHIFT_RUNTIME_DIR/srv/libmcrypt/lib"
+make install
 mkdir $OPENSHIFT_RUNTIME_DIR/srv/php/etc/apache2
-cd ..
-wget http://pecl.php.net/get/APC-3.1.13.tgz
-tar -zxf APC-3.1.13.tgz
-cd APC-3.1.13
-$OPENSHIFT_RUNTIME_DIR/srv/php/bin/phpize
-./configure \
---with-php-config=$OPENSHIFT_RUNTIME_DIR/srv/php/bin/php-config \
---enable-apc \
---enable-apc-debug=no
-make && make install
+
+#INSTALL APC (TEMPORARILY SUSPENDED)
+# cd ..
+# wget http://pecl.php.net/get/APC-3.1.13.tgz
+# tar -zxf APC-3.1.13.tgz
+# cd APC-3.1.13
+# $OPENSHIFT_RUNTIME_DIR/srv/php/bin/phpize
+# ./configure \
+# --with-php-config=$OPENSHIFT_RUNTIME_DIR/srv/php/bin/php-config \
+# --enable-apc \
+# --enable-apc-debug=no
+# make && make install
 
 # CLEANUP
 rm -r $OPENSHIFT_RUNTIME_DIR/tmp/*.tar.gz
